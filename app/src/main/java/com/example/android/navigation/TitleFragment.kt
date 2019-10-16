f15cddeb62e93df072b59132ca1d18b97adfb7da
Brianna Magnuson <54729913+Brianna-M-93@users.noreply.github.com>
2019-10-16 15:55:43 -0700

    changed the onClickListener code to a more simplified version where the listener is attached directly to the Navigation controller

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -19,9 +20,13 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
-        binding.playButton.setOnClickListener { view: View ->
-            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
-        }
+        binding.playButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+
+            //other variations of adding onClickListener to Navigation
+            //Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+            //view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         return binding.root
     }
 

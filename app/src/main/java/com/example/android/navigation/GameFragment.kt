357673234b72e2d92d6b399945b9cc94a2982580
Brianna Magnuson <54729913+Brianna-M-93@users.noreply.github.com>
2019-10-16 18:16:44 -0700

    added parameters to the Game Won fragment actions, added arguments to the fragment, added a Toast to see the questions answered correctly and the number of questions

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -98,7 +98,7 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
-                        view.findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment())
+                        view.findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment(numQuestions, questionIndex))
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
